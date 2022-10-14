0c2c6bca9eb472aeb442f02fb1f88b792939152a
kajal-max <kajal.mm@outlook.com>
2022-10-14 15:15:03 -0700

    status update

---
+++ task1.py
@@ -0,0 +1,80 @@
+# Question - 1
+# a,b,c = 1, 2.01,'String'
+# print(type(a)), print(type(b)), print(type(c))
+
+
+# Question - 2
+# x = 9j+9
+# print(type(x))
+# temp=x
+# x=print((int))
+
+
+# Question - 3
+# a = 10
+# b = 5
+# temp = a
+# a = b
+# b = temp
+# print("after swapping :")
+# print("value of a :",a)
+# print("value of b :",b)
+
+
+# a = 10
+# b = 5
+# a=a+b
+# b=a-b
+# a=a-b
+# print("after swapping:")
+# print("value of a:",a)
+# print("value of b:",b) 
+
+# Question - 4
+# print ("Hello world")
+
+
+# Question - 5
+# x = int(input("enter value for x ="))
+# y = int(input("enter value for y ="))
+# z = x+y
+# w = z + 30
+# print (w)
+
+
+# Question - 6
+# name = input("what is your name ?:")
+# age = int(input("Enter your age :"))
+# height = float(input("Enter your height :"))
+# x = int(input("Enter the number :"))
+# y = int(input("Enter the number:"))
+# a = int(input("Enter the number :"))
+# b = print(a)
+
+# print(F"your name is {name}")
+# print("The data type of the input value is", type(name))
+
+# print(F"your age is {age}")
+# print("The data type of the input value is", type(age))
+
+# print(F"your age is {height}")
+# print("The data type of the input value is", type(height))
+
+# print("The data type of the input value is", type(bool(x>y)))
+
+# print("The data type of the input value is", type(b))
+
+
+# Question - 7
+# CityName = "Upper camel case"
+# cityName = "Lower camel case"
+# City_name = "Snake case"
+# CITY_NAME = "Upper case"
+
+# Question - 8
+# Yes, variables in Python can be reassigned to a new value that 
+# is a different data type from its current value.
+# In fact, variables can be reassigned to any valid value in Python, regardless of its current value.
+# Variables are essentially like an empty box, that can contain something like a string,
+# number, or other value. When you assign it a value, the box will contain that value,
+# and when you reassign it, it will empty out the old value, and the new value will be placed inside of it.
